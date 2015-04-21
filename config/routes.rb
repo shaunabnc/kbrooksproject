9272e0317d511664c0bd5e9c4b2ebf4b0264b970
shaunabnc <kashauna.brooks@gmail.com>
2015-04-21 12:30:37 -0400

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get "welcome/coolpage"
+  root "welcome#coolpage"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
